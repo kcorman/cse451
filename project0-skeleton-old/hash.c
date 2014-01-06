303189d87814ebfa662e3c85e1afa19887746ab6
Kenny <kennywcorman@gmail.com>
2014-01-06 13:05:11 -0800

    Added old skeleton files so I can get started early

---
+++ project0-skeleton-old/hash.c
@@ -0,0 +1,20 @@
+/* Implements the abstract hash table. */
+
+#include <assert.h>
+#include <stdlib.h>
+
+#include "hash.h"
+
+/* Using an entry of this type is just a suggestion. You're
+ * free to change this. */
+typedef struct _hash_entry {
+  void* key;
+  void* value;
+} hash_entry;
+
+struct _hash_table {
+  /* TODO: add members to this struct! */
+};
+
+/* TODO: implement the functions in hash.h here! */
+
